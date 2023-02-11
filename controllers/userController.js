cff77674d0a83af53d4e5234915ff04cd09d06d9
Senthuran S <senthuran.vro@gmail.com>
2023-02-11 17:03:17 +0000

    Create User Controller
    - Added Route Handlers
    - Follows MVC Pattern

---
+++ controllers/userController.js
@@ -0,0 +1,36 @@
+//route handlers
+
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "Route is yet to be defined",
+  });
+};
+
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "Route is yet to be defined",
+  });
+};
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "Route is yet to be defined",
+  });
+};
+
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "Route is yet to be defined",
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "Route is yet to be defined",
+  });
+};
