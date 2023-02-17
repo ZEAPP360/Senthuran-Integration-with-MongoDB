2a5120ece7199d5e4e449969acc1b7202cdfca25
Senthuran S <senthuran.vro@gmail.com>
2023-02-17 12:22:46 +0000

    Add JWT Error Handlers

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -14,12 +14,20 @@ const handleDuplicateFieldsDB = (err) => {
   const message = `Duplicate field value: ${value}. Please use another value!`;
   return new AppError(message, 400);
 };
+//handles validation errors
 const handleValidationErrorDB = (err) => {
   const errors = Object.values(err.errors).map((el) => el.message);
 
   const message = `Invalid input data. ${errors.join(". ")}`;
   return new AppError(message, 400);
 };
+
+//handles JWT Errors for Invalid and Expired tokens
+const handleJWTError = () =>
+  new AppError("Invalid token. Please log in again!", 401);
+
+const handleJWTExpiredError = () =>
+  new AppError("Your token has expired! Please log in again.", 401);
 
 //errors for development mode
 const sendErrorDev = (err, res) => {
@@ -72,6 +80,8 @@ module.exports = (err, req, res, next) => {
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     if (error.name === "ValidationError")
       error = handleValidationErrorDB(error);
+    if (error.name === "JsonWebTokenError") error = handleJWTError();
+    if (error.name === "TokenExpiredError") error = handleJWTExpiredError();
 
     sendErrorProd(error, res);
   }
