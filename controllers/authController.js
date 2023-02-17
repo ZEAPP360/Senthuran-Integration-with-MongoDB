ae3067e884b4965d14cd90f6d85c081bfdea26c1
Senthuran S <senthuran.vro@gmail.com>
2023-02-17 10:12:42 +0000

    Create JSON Token for Sign Up

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,24 @@
 //contains all the handlers for authentication
+const jwt = require("jsonwebtoken");
 const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  //create JSON token - pass in payload, jwt secret, jwt expiraiton
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     status: "success",
+    token,
     data: {
       user: newUser,
     },
